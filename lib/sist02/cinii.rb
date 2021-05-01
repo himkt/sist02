9e7aa39d5edc32cc15dcfbe3a5b5dd7a4fd8e76b
eggplants <w10776e8w@yahoo.co.jp>
2021-05-01 15:43:50 +0900

    Kernel#open->URI#open

--- lib/sist02/cinii.rb
+++ lib/sist02/cinii.rb
@@ -6,7 +6,7 @@ module Sist02
   module CiNii
     # enable required program to call following functions
     module_function
-    
+
     def article_ref naid
 
       begin
@@ -31,7 +31,7 @@ module Sist02
         content = get_content url
         common_info = get_common_info content
         edition = content["prism:edition"]
-        ris = open("http://ci.nii.ac.jp/ncid/#{ncid}.ris").read
+        ris = URI.open("http://ci.nii.ac.jp/ncid/#{ncid}.ris").read
         edition = ris.match(/ET\s{2}-\s(.*)\r/) ? ris.match(/ET\s{2}-\s(.*)\r/)[1] : nil
         publisher = ris.match(/PB\s{2}-\s(.*)\r/) ? ris.match(/PB\s{2}-\s(.*)\r/)[1] : nil
         page = ris.match(/EP\s{2}-\s.*?(\d*?p)/) ? ris.match(/EP\s{2}-\s.*?(\d*?p)/)[1] : 'ページ数不明'
@@ -44,7 +44,7 @@ module Sist02
     end
 
     def dissertation_ref naid
-      
+
       begin
 
         url = "http://ci.nii.ac.jp/naid/#{naid}.json"
@@ -58,11 +58,11 @@ module Sist02
     end
 
     def get_content url
-      html = open(url).read
+      html = URI.open(url).read
       json = JSON.parser.new(html)
       json.parse["@graph"][0]
     end
-    
+
 
     def get_common_info content
       common_info = {}
